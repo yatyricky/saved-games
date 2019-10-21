41522c953d512139a0df88d975fe9ee4716ef381
Rick Sun <yatyricky@gmail.com>
2019-10-21 13:55:51 +0800

    auto commit

--- World of Warcraft/classic/YATYRICKY/木喉要塞/洛科林丶雷酒/SavedVariables/FiveSecondRule.lua
+++ World of Warcraft/classic/YATYRICKY/木喉要塞/洛科林丶雷酒/SavedVariables/FiveSecondRule.lua
@@ -7,18 +7,34 @@ FiveSecondRule_Options = {
 		0.35, -- [3]
 		0.8, -- [4]
 	},
-	["barWidth"] = 117,
+	["tickSizeRunningWindow"] = {
+		66, -- [1]
+		66, -- [2]
+		66, -- [3]
+		66, -- [4]
+		66, -- [5]
+		66, -- [6]
+		66, -- [7]
+		67, -- [8]
+		66, -- [9]
+		65, -- [10]
+	},
+	["barHeight"] = 11,
+	["averageManaTick"] = 66,
 	["barTop"] = -68,
-	["averageManaTick"] = 65.9,
-	["unlocked"] = false,
 	["statusBarBackgroundColor"] = {
 		0, -- [1]
 		0, -- [2]
 		0, -- [3]
 		0.55, -- [4]
 	},
-	["flat"] = false,
-	["showSpark"] = true,
+	["barLeft"] = 90,
+	["manaTicksColor"] = {
+		0.95, -- [1]
+		0.95, -- [2]
+		0.95, -- [3]
+		1, -- [4]
+	},
 	["showText"] = true,
 	["statusBarColor"] = {
 		0, -- [1]
@@ -26,24 +42,8 @@ FiveSecondRule_Options = {
 		1, -- [3]
 		0.95, -- [4]
 	},
-	["manaTicksColor"] = {
-		0.95, -- [1]
-		0.95, -- [2]
-		0.95, -- [3]
-		1, -- [4]
-	},
-	["barLeft"] = 90,
-	["barHeight"] = 11,
-	["tickSizeRunningWindow"] = {
-		66, -- [1]
-		66, -- [2]
-		67, -- [3]
-		66, -- [4]
-		65, -- [5]
-		66, -- [6]
-		66, -- [7]
-		66, -- [8]
-		66, -- [9]
-		65, -- [10]
-	},
+	["showSpark"] = true,
+	["flat"] = false,
+	["unlocked"] = false,
+	["barWidth"] = 117,
 }
