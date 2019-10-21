3c8d6bf0ab94fde3108625507acd8e85c2c9f640
Rick Sun <yatyricky@gmail.com>
2019-10-21 08:28:52 +0800

    auto commit

---
+++ World of Warcraft/classic/YATYRICKY/木喉要塞/洛科林丶雷酒/SavedVariables/FiveSecondRule.lua
@@ -0,0 +1,49 @@
+
+FiveSecondRule_Options = {
+	["showTicks"] = true,
+	["manaTicksBackgroundColor"] = {
+		0.35, -- [1]
+		0.35, -- [2]
+		0.35, -- [3]
+		0.8, -- [4]
+	},
+	["barWidth"] = 117,
+	["barTop"] = -68,
+	["averageManaTick"] = 65.9,
+	["unlocked"] = false,
+	["statusBarBackgroundColor"] = {
+		0, -- [1]
+		0, -- [2]
+		0, -- [3]
+		0.55, -- [4]
+	},
+	["flat"] = false,
+	["showSpark"] = true,
+	["showText"] = true,
+	["statusBarColor"] = {
+		0, -- [1]
+		0, -- [2]
+		1, -- [3]
+		0.95, -- [4]
+	},
+	["manaTicksColor"] = {
+		0.95, -- [1]
+		0.95, -- [2]
+		0.95, -- [3]
+		1, -- [4]
+	},
+	["barLeft"] = 90,
+	["barHeight"] = 11,
+	["tickSizeRunningWindow"] = {
+		66, -- [1]
+		66, -- [2]
+		67, -- [3]
+		66, -- [4]
+		65, -- [5]
+		66, -- [6]
+		66, -- [7]
+		66, -- [8]
+		66, -- [9]
+		65, -- [10]
+	},
+}
