3681c92df02bd272e2d77629eb2da4287628a9ce
Rick Sun <yatyricky@gmail.com>
2023-05-12 12:37:39 +0800

    git commit allow empty

--- d2r.js
+++ d2r.js
@@ -35,7 +35,7 @@ async function main() {
     console.log("Upload data")
     let changedFiles = await funcs.shell("git diff --name-only")
     await funcs.shell("git add -A");
-    await funcs.shell(`git commit -am "auto commit ${changedFiles.split("\n").filter(e => e.trim().length > 0).join(", ")}"`);
+    await funcs.shell(`git commit --allow-empty -m "auto commit ${changedFiles.split("\n").filter(e => e.trim().length > 0).join(", ")}"`);
     await funcs.shell("git push");
 }
 
