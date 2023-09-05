70ef2d4cb6eaf9aab0cfb78867a35c19e20e6e44
Rick Sun <yatyricky@gmail.com>
2023-09-05 21:59:13 +0800

    auto commit

--- World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Custom_Item_Notes.lua
+++ World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Custom_Item_Notes.lua
@@ -10,20 +10,32 @@ CIN_Save = {
 		"Thousand Needles", -- [1]
 		"Safe to delete.  way 77.32 74.29 Entrance to Razzeric to get a replacement", -- [2]
 	},
+	["21237"] = {
+		"Vanilla TAQ weapon quest. Needs 3 Elementium Ingots.", -- [1]
+	},
 	["162571"] = {
 		"BFA quest", -- [1]
 	},
-	["20930"] = {
-		"TAQ. Helmet for Pal, Rog, Dru, SM, Hun", -- [1]
+	["20932"] = {
+		"VANILLA. TAQ T2.5 Shoulder & Boots for Paladin, Shaman, Mage, Warlock, Druid", -- [1]
+	},
+	["52025"] = {
+		"WLK T10 token", -- [1]
+	},
+	["20933"] = {
+		"Vanilla TAQ T2.5 Armor for Priest, Mage, Warlock, Druid", -- [1]
 	},
 	["71617"] = {
 		"upgrade fireland items to heroic. drop in heroic only.", -- [1]
 	},
+	["207030"] = {
+		"DF. Thaldraszus /way #2025 51.0 56.6 Shi Everbreeze", -- [1]
+	},
 	["13159"] = {
 		"trash item", -- [1]
 	},
-	["162580"] = {
-		"BFA quest", -- [1]
+	["203914"] = {
+		"DF. Secrets of a lost empire. Trade old mog and stuff.", -- [1]
 	},
 	["71634"] = {
 		"Azure Span /way #2024 16.57, 38.31", -- [1]
@@ -35,11 +47,11 @@ CIN_Save = {
 		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
 		"The Azure Span", -- [2]
 	},
-	["162584"] = {
-		"BFA quest", -- [1]
+	["190456"] = {
+		"DF currency for recepies", -- [1]
 	},
-	["71141"] = {
-		"Time cavern. Firelands quest item", -- [1]
+	["162581"] = {
+		"BFA quest", -- [1]
 	},
 	["202017"] = {
 		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
@@ -51,26 +63,26 @@ CIN_Save = {
 	["204195"] = {
 		"DF. Upgrade NPC is at Zaralak Cavern or Loamm", -- [1]
 	},
-	["207030"] = {
-		"DF. Thaldraszus /way #2025 51.0 56.6 Shi Everbreeze", -- [1]
+	["162580"] = {
+		"BFA quest", -- [1]
 	},
 	["204193"] = {
 		"DF. Upgrade NPC is at Zaralak Cavern or Loamm", -- [1]
 	},
-	["32897"] = {
-		"trash", -- [1]
+	["20930"] = {
+		"VANILLA. TAQ T2.5 Helmet for Paladin, Hunter, Rogue, Shaman, Druid", -- [1]
 	},
 	["13158"] = {
 		"trash https://www.wowhead.com/item=13158", -- [1]
 	},
-	["162581"] = {
+	["32897"] = {
+		"trash", -- [1]
+	},
+	["71141"] = {
+		"Time cavern. Firelands quest item", -- [1]
+	},
+	["162584"] = {
 		"BFA quest", -- [1]
-	},
-	["190456"] = {
-		"DF currency for recepies", -- [1]
-	},
-	["203914"] = {
-		"DF. Secrets of a lost empire. Trade old mog and stuff.", -- [1]
 	},
 	["21229"] = {
 		"TAQ rep", -- [1]
