ef786f3ef0d5af5cfafffc2e694fff5ca49c2246
Rick Sun <yatyricky@gmail.com>
2023-09-05 14:25:01 +0800

    auto commit

--- World_of_Warcraft/retail/WTF/Account/250607394#1/Azshara/Ziadwan/SavedVariables/Blizzard_Professions.lua
+++ World_of_Warcraft/retail/WTF/Account/250607394#1/Azshara/Ziadwan/SavedVariables/Blizzard_Professions.lua
@@ -4,6 +4,6 @@ g_professionsSpecsSelectedTabs = {
 	[2834] = 403,
 }
 g_professionsSpecsSelectedPaths = {
-	[350] = 28545,
-	[403] = 34688,
+	[350] = 28546,
+	[403] = 34689,
 }
--- World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Custom_Item_Notes.lua
+++ World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Custom_Item_Notes.lua
@@ -13,14 +13,17 @@ CIN_Save = {
 	["162571"] = {
 		"BFA quest", -- [1]
 	},
-	["190456"] = {
-		"DF currency for recepies", -- [1]
+	["20930"] = {
+		"TAQ. Helmet for Pal, Rog, Dru, SM, Hun", -- [1]
+	},
+	["71617"] = {
+		"upgrade fireland items to heroic. drop in heroic only.", -- [1]
 	},
 	["13159"] = {
 		"trash item", -- [1]
 	},
-	["71617"] = {
-		"upgrade fireland items to heroic. drop in heroic only.", -- [1]
+	["162580"] = {
+		"BFA quest", -- [1]
 	},
 	["71634"] = {
 		"Azure Span /way #2024 16.57, 38.31", -- [1]
@@ -32,11 +35,11 @@ CIN_Save = {
 		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
 		"The Azure Span", -- [2]
 	},
-	["204193"] = {
-		"DF. Upgrade NPC is at Zaralak Cavern or Loamm", -- [1]
+	["162584"] = {
+		"BFA quest", -- [1]
 	},
-	["207030"] = {
-		"DF. Thaldraszus /way #2025 51.0 56.6 Shi Everbreeze", -- [1]
+	["71141"] = {
+		"Time cavern. Firelands quest item", -- [1]
 	},
 	["202017"] = {
 		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
@@ -48,26 +51,26 @@ CIN_Save = {
 	["204195"] = {
 		"DF. Upgrade NPC is at Zaralak Cavern or Loamm", -- [1]
 	},
-	["162580"] = {
-		"BFA quest", -- [1]
+	["207030"] = {
+		"DF. Thaldraszus /way #2025 51.0 56.6 Shi Everbreeze", -- [1]
 	},
-	["13158"] = {
-		"trash https://www.wowhead.com/item=13158", -- [1]
+	["204193"] = {
+		"DF. Upgrade NPC is at Zaralak Cavern or Loamm", -- [1]
 	},
 	["32897"] = {
 		"trash", -- [1]
 	},
+	["13158"] = {
+		"trash https://www.wowhead.com/item=13158", -- [1]
+	},
 	["162581"] = {
 		"BFA quest", -- [1]
 	},
+	["190456"] = {
+		"DF currency for recepies", -- [1]
+	},
 	["203914"] = {
 		"DF. Secrets of a lost empire. Trade old mog and stuff.", -- [1]
-	},
-	["71141"] = {
-		"Time cavern. Firelands quest item", -- [1]
-	},
-	["162584"] = {
-		"BFA quest", -- [1]
 	},
 	["21229"] = {
 		"TAQ rep", -- [1]
