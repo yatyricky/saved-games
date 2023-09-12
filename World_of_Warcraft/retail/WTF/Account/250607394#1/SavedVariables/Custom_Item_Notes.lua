a57bf966496effc8bfa9e37a29be09e3bb00e926
Rick Sun <yatyricky@gmail.com>
2023-09-12 16:49:14 +0800

    auto commit

--- World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Custom_Item_Notes.lua
+++ World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Custom_Item_Notes.lua
@@ -1,26 +1,365 @@
 
 CIN_Save = {
+	["3685"] = {
+		"Vanilla", -- [1]
+	},
+	["3667"] = {
+		"Vanilla", -- [1]
+	},
+	["17056"] = {
+		"Vanilla", -- [1]
+	},
+	["2589"] = {
+		"Vanilla", -- [1]
+	},
+	["4232"] = {
+		"Vanilla", -- [1]
+	},
+	["21153"] = {
+		"Vanilla", -- [1]
+	},
+	["2678"] = {
+		"Vanilla", -- [1]
+	},
+	["3731"] = {
+		"Vanilla", -- [1]
+	},
+	["159"] = {
+		"Vanilla", -- [1]
+	},
+	["7912"] = {
+		"Vanilla", -- [1]
+	},
+	["8170"] = {
+		"Vanilla", -- [1]
+	},
+	["14047"] = {
+		"Vanilla", -- [1]
+	},
+	["203914"] = {
+		"DF. Secrets of a lost empire. Trade old mog and stuff.", -- [1]
+	},
+	["15410"] = {
+		"Vanilla", -- [1]
+		"LW", -- [2]
+	},
+	["14341"] = {
+		"Vanilla", -- [1]
+	},
+	["71617"] = {
+		"upgrade fireland items to heroic. drop in heroic only.", -- [1]
+	},
+	["4306"] = {
+		"Vanilla", -- [1]
+	},
+	["2450"] = {
+	},
+	["774"] = {
+		"Vanilla", -- [1]
+	},
+	["13159"] = {
+		"trash item", -- [1]
+	},
+	["6289"] = {
+		"Vanilla", -- [1]
+	},
+	["1080"] = {
+		"Vanilla", -- [1]
+	},
+	["5468"] = {
+		"Vanilla", -- [1]
+	},
+	["5465"] = {
+		"Vanilla", -- [1]
+	},
+	["12203"] = {
+		"Vanilla", -- [1]
+		"Cooking", -- [2]
+	},
+	["15419"] = {
+		"Vanilla", -- [1]
+		"LW", -- [2]
+	},
+	["30817"] = {
+		"Vanilla", -- [1]
+	},
+	["1468"] = {
+		"Vanilla", -- [1]
+	},
+	["15412"] = {
+		"Vanilla", -- [1]
+	},
 	["200093"] = {
 		"DF", -- [1]
 	},
-	["20884"] = {
-		"Vanilla, RAQ ring for Warrior, Paladin, Shaman, Mage, Druid", -- [1]
+	["19441"] = {
+		"Vanilla", -- [1]
+		"Blacksmith, Alchemy", -- [2]
+	},
+	["5635"] = {
+		"Vanilla", -- [1]
 	},
 	["52025"] = {
 		"WLK T10 token", -- [1]
 	},
-	["81055"] = {
-		"DMF WHEE! +10% rep & exp", -- [1]
-	},
 	["45798"] = {
 		"WLK ULD. No longer needed. Keep as a trophy.", -- [1]
+	},
+	["1179"] = {
+		"Vanilla", -- [1]
+	},
+	["12204"] = {
+		"Vanilla", -- [1]
+	},
+	["12208"] = {
+		"Vanilla", -- [1]
+	},
+	["2251"] = {
+		"Vanilla", -- [1]
+	},
+	["22644"] = {
+		"Vanilla", -- [1]
+		"Cooking", -- [2]
+	},
+	["6303"] = {
+		"Vanilla", -- [1]
+	},
+	["12037"] = {
+		"Vanilla", -- [1]
+	},
+	["2452"] = {
+		"Vanilla", -- [1]
+	},
+	["45040"] = {
+		"Darkshore Shatterspear War Camp quest, bugged. Kill Rit'ko to regain this item.", -- [1]
+	},
+	["2677"] = {
+		"Vanilla", -- [1]
+	},
+	["55121"] = {
+		"Thousand Needles", -- [1]
+		"Safe to delete.  way 77.32 74.29 Entrance to Razzeric to get a replacement", -- [2]
+	},
+	["5504"] = {
+		"Vanilla", -- [1]
+	},
+	["20932"] = {
+		"VANILLA. TAQ T2.5 Shoulder & Boots for Paladin, Shaman, Mage, Warlock, Druid", -- [1]
+	},
+	["723"] = {
+		"Vanilla", -- [1]
+	},
+	["17012"] = {
+		"Vanilla", -- [1]
+		"LW, Tailor, Smith", -- [2]
+	},
+	["22682"] = {
+		"Vanilla", -- [1]
+	},
+	["204195"] = {
+		"DF. Upgrade NPC is at Zaralak Cavern or Loamm", -- [1]
+	},
+	["162581"] = {
+		"BFA quest", -- [1]
+	},
+	["16202"] = {
+		"Vanilla", -- [1]
+	},
+	["190456"] = {
+		"DF currency for recepies", -- [1]
+	},
+	["21229"] = {
+		"TAQ rep", -- [1]
+	},
+	["5503"] = {
+		"Vanilla", -- [1]
+	},
+	["4234"] = {
+		"Vanilla", -- [1]
+	},
+	["12205"] = {
+		"Vanilla", -- [1]
+	},
+	["13760"] = {
+		"Vanilla", -- [1]
+	},
+	["6361"] = {
+		"Vanilla", -- [1]
+	},
+	["7075"] = {
+		"Vanilla", -- [1]
+	},
+	["2674"] = {
+		"Vanilla", -- [1]
 	},
 	["71634"] = {
 		"Azure Span /way #2024 16.57, 38.31", -- [1]
 	},
-	["202018"] = {
-		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
-		"The Azure Span", -- [2]
+	["7974"] = {
+		"Vanilla", -- [1]
+	},
+	["19943"] = {
+		"Vanilla", -- [1]
+	},
+	["5498"] = {
+		"Vanilla", -- [1]
+	},
+	["6308"] = {
+		"Vanilla", -- [1]
+	},
+	["13158"] = {
+		"trash https://www.wowhead.com/item=13158", -- [1]
+	},
+	["32897"] = {
+		"trash", -- [1]
+	},
+	["7076"] = {
+		"Vanilla", -- [1]
+	},
+	["21071"] = {
+		"Vanilla", -- [1]
+	},
+	["2770"] = {
+		"Vanilla", -- [1]
+	},
+	["20520"] = {
+		"Vanilla", -- [1]
+	},
+	["20424"] = {
+		"Vanilla", -- [1]
+	},
+	["6522"] = {
+		"Vanilla", -- [1]
+	},
+	["2325"] = {
+		"Vanilla", -- [1]
+	},
+	["67229"] = {
+		"Vanilla", -- [1]
+	},
+	["4603"] = {
+		"Vanilla", -- [1]
+	},
+	["8172"] = {
+		"Vanilla", -- [1]
+	},
+	["1015"] = {
+		"Vanilla", -- [1]
+	},
+	["12207"] = {
+		"Vanilla", -- [1]
+	},
+	["4304"] = {
+		"Vanilla", -- [1]
+	},
+	["818"] = {
+		"Vanilla", -- [1]
+	},
+	["6310"] = {
+		"Vanilla", -- [1]
+	},
+	["2592"] = {
+		"Vanilla", -- [1]
+	},
+	["4338"] = {
+		"Vanilla", -- [1]
+	},
+	["1475"] = {
+		"Vanilla", -- [1]
+	},
+	["12811"] = {
+		"Vanilla", -- [1]
+	},
+	["8154"] = {
+		"Vanilla. LW Tough Scorpid set. Thick Leather + This + Silk Thread", -- [1]
+	},
+	["12804"] = {
+		"Vanilla", -- [1]
+	},
+	["204193"] = {
+		"DF. Upgrade NPC is at Zaralak Cavern or Loamm", -- [1]
+	},
+	["8153"] = {
+		"Vanilla", -- [1]
+	},
+	["12810"] = {
+		"Vanilla", -- [1]
+	},
+	["12662"] = {
+		"Vanilla", -- [1]
+	},
+	["71141"] = {
+		"Time cavern. Firelands quest item", -- [1]
+	},
+	["17010"] = {
+		"Vanilla", -- [1]
+	},
+	["769"] = {
+		"Vanilla", -- [1]
+	},
+	["17011"] = {
+		"Vanilla", -- [1]
+	},
+	["7069"] = {
+		"Vanilla", -- [1]
+	},
+	["7972"] = {
+		"Vanilla", -- [1]
+	},
+	["7080"] = {
+		"Vanilla", -- [1]
+	},
+	["7079"] = {
+		"Vanilla", -- [1]
+	},
+	["20884"] = {
+		"Vanilla, RAQ ring for Warrior, Paladin, Shaman, Mage, Druid", -- [1]
+	},
+	["7971"] = {
+		"Vanilla", -- [1]
+	},
+	["2449"] = {
+		"Vanilla", -- [1]
+	},
+	["765"] = {
+		"Vanilla", -- [1]
+	},
+	["81055"] = {
+		"DMF WHEE! +10% rep & exp", -- [1]
+	},
+	["7067"] = {
+		"Vanilla", -- [1]
+	},
+	["7077"] = {
+		"Vanilla", -- [1]
+	},
+	["10286"] = {
+		"Vanilla", -- [1]
+	},
+	["7078"] = {
+		"Vanilla", -- [1]
+	},
+	["12803"] = {
+		"Vanilla", -- [1]
+	},
+	["8171"] = {
+		"Vanilla", -- [1]
+	},
+	["2675"] = {
+		"Vanilla", -- [1]
+	},
+	["5470"] = {
+		"Vanilla", -- [1]
+		"Cooking", -- [2]
+	},
+	["2924"] = {
+		"Vanilla", -- [1]
+	},
+	["12206"] = {
+		"Vanilla", -- [1]
+	},
+	["2842"] = {
+		"Vanilla", -- [1]
 	},
 	["202017"] = {
 		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
@@ -29,23 +368,52 @@ CIN_Save = {
 	["45079"] = {
 		"safe to delete after loch modan quest complete", -- [1]
 	},
+	["35562"] = {
+		"Vanilla", -- [1]
+	},
+	["2319"] = {
+		"Vanilla", -- [1]
+	},
+	["17203"] = {
+		"Vanilla", -- [1]
+	},
+	["15416"] = {
+		"Vanilla", -- [1]
+	},
+	["20933"] = {
+		"Vanilla TAQ T2.5 Armor for Priest, Mage, Warlock, Druid", -- [1]
+	},
+	["3173"] = {
+		"Vanilla", -- [1]
+	},
+	["18562"] = {
+		"Vanilla", -- [1]
+	},
+	["8150"] = {
+		"Vanilla", -- [1]
+		"LW, Cook, Engineer", -- [2]
+	},
+	["3857"] = {
+		"Vanilla", -- [1]
+	},
 	["207030"] = {
 		"DF. Thaldraszus /way #2025 51.0 56.6 Shi Everbreeze", -- [1]
 	},
-	["13158"] = {
-		"trash https://www.wowhead.com/item=13158", -- [1]
+	["13755"] = {
+		"Vanilla", -- [1]
 	},
-	["32897"] = {
-		"trash", -- [1]
+	["14256"] = {
+		"Vanilla", -- [1]
 	},
-	["203914"] = {
-		"DF. Secrets of a lost empire. Trade old mog and stuff.", -- [1]
+	["15407"] = {
+		"Vanilla", -- [1]
 	},
-	["45040"] = {
-		"Darkshore Shatterspear War Camp quest, bugged. Kill Rit'ko to regain this item.", -- [1]
+	["5637"] = {
+		"Vanilla", -- [1]
+		"LW, Smith, Jewel, Alchemy, Enchant", -- [2]
 	},
-	["71617"] = {
-		"upgrade fireland items to heroic. drop in heroic only.", -- [1]
+	["8165"] = {
+		"Vanilla", -- [1]
 	},
 	["200613"] = {
 		"DF, Teleport to Oharan centuar's camp", -- [1]
@@ -56,14 +424,10 @@ CIN_Save = {
 	["162571"] = {
 		"BFA quest", -- [1]
 	},
-	["20932"] = {
-		"VANILLA. TAQ T2.5 Shoulder & Boots for Paladin, Shaman, Mage, Warlock, Druid", -- [1]
-	},
-	["13159"] = {
-		"trash item", -- [1]
-	},
-	["8154"] = {
-		"Vanilla. LW Tough Scorpid set. Thick Leather + This + Silk Thread", -- [1]
+	["1288"] = {
+		"Vanilla", -- [1]
+		"Alchemy", -- [2]
+		"trash", -- [3]
 	},
 	["204985"] = {
 		"DF", -- [1]
@@ -71,45 +435,66 @@ CIN_Save = {
 	["20890"] = {
 		"Vanilla RAQ weapon for Priest, Mage, Warlock, Druid", -- [1]
 	},
-	["55121"] = {
-		"Thousand Needles", -- [1]
-		"Safe to delete.  way 77.32 74.29 Entrance to Razzeric to get a replacement", -- [2]
+	["12184"] = {
+		"Vanilla", -- [1]
 	},
-	["21232"] = {
-		"Vanilla TAQ weapon quest. Needs 3 elementium ingots", -- [1]
+	["2672"] = {
+		"Vanilla", -- [1]
+	},
+	["2318"] = {
+		"Vanilla", -- [1]
+	},
+	["4402"] = {
+		"Vanilla", -- [1]
+	},
+	["3712"] = {
+		"Vanilla", -- [1]
+	},
+	["2596"] = {
+		"Vanilla", -- [1]
+	},
+	["6889"] = {
+		"Vanilla", -- [1]
+	},
+	["202018"] = {
+		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
+		"The Azure Span", -- [2]
+	},
+	["13759"] = {
+		"Vanilla", -- [1]
 	},
 	["20931"] = {
 		"Vanilla T2.5 pants for Paladin, Hunter, Shaman, Warlock, Druid", -- [1]
 	},
-	["162581"] = {
-		"BFA quest", -- [1]
+	["6291"] = {
+		"Vanilla", -- [1]
 	},
-	["204195"] = {
-		"DF. Upgrade NPC is at Zaralak Cavern or Loamm", -- [1]
+	["2886"] = {
+		"Vanilla", -- [1]
 	},
-	["162580"] = {
-		"BFA quest", -- [1]
+	["7081"] = {
+		"Vanilla", -- [1]
 	},
-	["204193"] = {
-		"DF. Upgrade NPC is at Zaralak Cavern or Loamm", -- [1]
+	["12202"] = {
+		"Vanilla", -- [1]
 	},
 	["20930"] = {
 		"VANILLA. TAQ T2.5 Helmet for Paladin, Hunter, Rogue, Shaman, Druid", -- [1]
 	},
-	["20933"] = {
-		"Vanilla TAQ T2.5 Armor for Priest, Mage, Warlock, Druid", -- [1]
+	["2673"] = {
+		"Vanilla", -- [1]
 	},
-	["190456"] = {
-		"DF currency for recepies", -- [1]
+	["3404"] = {
+		"Vanilla", -- [1]
 	},
-	["71141"] = {
-		"Time cavern. Firelands quest item", -- [1]
+	["21232"] = {
+		"Vanilla TAQ weapon quest. Needs 3 elementium ingots", -- [1]
 	},
 	["162584"] = {
 		"BFA quest", -- [1]
 	},
-	["21229"] = {
-		"TAQ rep", -- [1]
+	["162580"] = {
+		"BFA quest", -- [1]
 	},
 	["199906"] = {
 		"DF Turned in at Sorotis (in Valdrakken - 26,40)", -- [1]
