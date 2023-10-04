2ba124b17e09672596b0a76ea4bd43e7127e2815
Rick Sun <yatyricky@gmail.com>
2023-10-04 15:34:08 +0800

    auto commit

--- World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Incognito.lua
+++ World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Incognito.lua
@@ -3,8 +3,8 @@ IncognitoDB = {
 	["profileKeys"] = {
 		["Ziadwan - Azshara"] = "Default",
 		["Gamwillsaveu - Azshara"] = "Default",
-		["Dahoontar - Azshara"] = "Default",
 		["Bblilmao - Azshara"] = "Default",
+		["Dahoontar - Azshara"] = "Default",
 	},
 	["profiles"] = {
 		["Default"] = {
