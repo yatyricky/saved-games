619e7b309982726bca75f15f5a1eeb3ee3ba0812
Rick Sun <yatyricky@gmail.com>
2023-12-31 13:50:31 +0800

    auto commit

--- World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Custom_Item_Notes.lua
+++ World_of_Warcraft/retail/WTF/Account/250607394#1/SavedVariables/Custom_Item_Notes.lua
@@ -174,9 +174,8 @@ CIN_Save = {
 	["21882"] = {
 		"TBC", -- [1]
 	},
-	["24291"] = {
-		"TBC", -- [1]
-		"sporeman rep turn in", -- [2]
+	["765"] = {
+		"Vanilla", -- [1]
 	},
 	["20879"] = {
 		"Vanilla", -- [1]
@@ -191,13 +190,13 @@ CIN_Save = {
 	["22682"] = {
 		"Vanilla", -- [1]
 	},
-	["12804"] = {
+	["4338"] = {
 		"Vanilla", -- [1]
 	},
-	["12662"] = {
+	["7069"] = {
 		"Vanilla", -- [1]
 	},
-	["20862"] = {
+	["7080"] = {
 		"Vanilla", -- [1]
 	},
 	["6310"] = {
@@ -215,10 +214,10 @@ CIN_Save = {
 	["162581"] = {
 		"BFA quest", -- [1]
 	},
-	["7081"] = {
-		"Vanilla", -- [1]
+	["162580"] = {
+		"BFA quest", -- [1]
 	},
-	["2673"] = {
+	["17010"] = {
 		"Vanilla", -- [1]
 	},
 	["16202"] = {
@@ -233,31 +232,30 @@ CIN_Save = {
 	["21229"] = {
 		"TAQ rep", -- [1]
 	},
-	["5503"] = {
-		"Vanilla", -- [1]
-	},
 	["192615"] = {
 		"DF", -- [1]
 		"used in a five day turn-in quest chain to get the mount Lizi's Reins.", -- [2]
 		"Renown 9 with Maruuk Centaur", -- [3]
 	},
-	["13759"] = {
+	["5503"] = {
 		"Vanilla", -- [1]
 	},
-	["2842"] = {
+	["21232"] = {
+		"Vanilla TAQ weapon quest. Needs 3 elementium ingots", -- [1]
+	},
+	["7078"] = {
 		"Vanilla", -- [1]
 	},
-	["6291"] = {
+	["6889"] = {
 		"Vanilla", -- [1]
 	},
-	["202018"] = {
-		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
-		"The Azure Span", -- [2]
+	["2449"] = {
+		"Vanilla", -- [1]
 	},
 	["4234"] = {
 		"Vanilla", -- [1]
 	},
-	["3712"] = {
+	["20864"] = {
 		"Vanilla", -- [1]
 	},
 	["12205"] = {
@@ -269,11 +267,11 @@ CIN_Save = {
 	["13760"] = {
 		"Vanilla", -- [1]
 	},
-	["2596"] = {
-		"Vanilla", -- [1]
-	},
 	["3404"] = {
 		"Vanilla", -- [1]
+	},
+	["22450"] = {
+		"TBC", -- [1]
 	},
 	["6361"] = {
 		"Vanilla", -- [1]
@@ -284,23 +282,22 @@ CIN_Save = {
 	["2674"] = {
 		"Vanilla", -- [1]
 	},
-	["71634"] = {
-		"Azure Span /way #2024 16.57, 38.31", -- [1]
-	},
-	["12184"] = {
+	["2596"] = {
 		"Vanilla", -- [1]
 	},
-	["17203"] = {
+	["20860"] = {
 		"Vanilla", -- [1]
 	},
-	["24245"] = {
-		"TBC", -- [1]
-		"sporeman rep turn in", -- [2]
+	["20424"] = {
+		"Vanilla", -- [1]
+	},
+	["2325"] = {
+		"Vanilla", -- [1]
 	},
 	["7974"] = {
 		"Vanilla", -- [1]
 	},
-	["7079"] = {
+	["20865"] = {
 		"Vanilla", -- [1]
 	},
 	["19943"] = {
@@ -324,23 +321,24 @@ CIN_Save = {
 	["21071"] = {
 		"Vanilla", -- [1]
 	},
-	["2770"] = {
+	["17203"] = {
 		"Vanilla", -- [1]
 	},
 	["20520"] = {
 		"Vanilla", -- [1]
 	},
-	["162571"] = {
-		"BFA quest", -- [1]
+	["199219"] = {
+		"DF", -- [1]
+		"Buy Chip @ Waking Shores /way #2022 26.4 55.4 Chip", -- [2]
 	},
 	["22446"] = {
 		"TBC", -- [1]
 	},
-	["5470"] = {
-		"Vanilla", -- [1]
-		"Cooking", -- [2]
+	["24245"] = {
+		"TBC", -- [1]
+		"sporeman rep turn in", -- [2]
 	},
-	["4603"] = {
+	["20872"] = {
 		"Vanilla", -- [1]
 	},
 	["6522"] = {
@@ -349,20 +347,22 @@ CIN_Save = {
 	["207002"] = {
 		"DF", -- [1]
 	},
-	["22449"] = {
-		"TBC", -- [1]
+	["7079"] = {
+		"Vanilla", -- [1]
 	},
 	["67229"] = {
 		"Vanilla", -- [1]
 	},
-	["20872"] = {
+	["4603"] = {
 		"Vanilla", -- [1]
 	},
-	["5471"] = {
-		"Vanilla. Cooking", -- [1]
+	["1288"] = {
+		"Vanilla", -- [1]
+		"Alchemy", -- [2]
+		"trash", -- [3]
 	},
-	["21877"] = {
-		"TBC", -- [1]
+	["8165"] = {
+		"Vanilla", -- [1]
 	},
 	["8172"] = {
 		"Vanilla", -- [1]
@@ -376,40 +376,37 @@ CIN_Save = {
 	["12207"] = {
 		"Vanilla", -- [1]
 	},
-	["1288"] = {
-		"Vanilla", -- [1]
-		"Alchemy", -- [2]
-		"trash", -- [3]
+	["5471"] = {
+		"Vanilla. Cooking", -- [1]
 	},
-	["4304"] = {
+	["12803"] = {
 		"Vanilla", -- [1]
 	},
 	["818"] = {
 		"Vanilla", -- [1]
 	},
-	["2318"] = {
+	["20873"] = {
 		"Vanilla", -- [1]
 	},
 	["15407"] = {
 		"Vanilla", -- [1]
 	},
-	["13755"] = {
+	["2886"] = {
+		"Vanilla", -- [1]
+	},
+	["2592"] = {
+		"Vanilla", -- [1]
+	},
+	["16203"] = {
+		"VANILLA", -- [1]
+	},
+	["12811"] = {
 		"Vanilla", -- [1]
 	},
 	["7972"] = {
 		"Vanilla", -- [1]
 	},
-	["18945"] = {
-		"Vanilla", -- [1]
-		"Turn in at Master Smith Burninate at Searing Gorge", -- [2]
-	},
-	["12811"] = {
-		"Vanilla", -- [1]
-	},
-	["8154"] = {
-		"Vanilla. LW Tough Scorpid set. Thick Leather + This + Silk Thread", -- [1]
-	},
-	["10286"] = {
+	["769"] = {
 		"Vanilla", -- [1]
 	},
 	["204193"] = {
@@ -421,29 +418,30 @@ CIN_Save = {
 	["12810"] = {
 		"Vanilla", -- [1]
 	},
-	["20863"] = {
-		"Vanilla", -- [1]
+	["22457"] = {
+		"TBC", -- [1]
 	},
 	["71141"] = {
 		"Time cavern. Firelands quest item", -- [1]
 	},
-	["16203"] = {
-		"VANILLA", -- [1]
+	["18945"] = {
+		"Vanilla", -- [1]
+		"Turn in at Master Smith Burninate at Searing Gorge", -- [2]
 	},
-	["769"] = {
+	["10286"] = {
 		"Vanilla", -- [1]
 	},
 	["17011"] = {
 		"Vanilla", -- [1]
 	},
-	["33865"] = {
-		"TBC,CTM", -- [1]
-		"Zulaman", -- [2]
-	},
-	["4402"] = {
+	["20863"] = {
 		"Vanilla", -- [1]
 	},
-	["18562"] = {
+	["5637"] = {
+		"Vanilla", -- [1]
+		"LW, Smith, Jewel, Alchemy, Enchant", -- [2]
+	},
+	["6358"] = {
 		"Vanilla", -- [1]
 	},
 	["20890"] = {
@@ -455,11 +453,11 @@ CIN_Save = {
 	["7971"] = {
 		"Vanilla", -- [1]
 	},
-	["20870"] = {
+	["2672"] = {
 		"Vanilla", -- [1]
 	},
-	["128860"] = {
-		"LEG", -- [1]
+	["20870"] = {
+		"Vanilla", -- [1]
 	},
 	["81055"] = {
 		"DMF WHEE! +10% rep & exp", -- [1]
@@ -471,11 +469,12 @@ CIN_Save = {
 		"Vanilla", -- [1]
 		"LW, Cook, Engineer", -- [2]
 	},
-	["45079"] = {
-		"safe to delete after loch modan quest complete", -- [1]
-	},
 	["20861"] = {
 		"Vanilla", -- [1]
+	},
+	["32569"] = {
+		"TBC", -- [1]
+		"Ogrila blade's edge", -- [2]
 	},
 	["204985"] = {
 		"DF", -- [1]
@@ -486,8 +485,8 @@ CIN_Save = {
 	["2675"] = {
 		"Vanilla", -- [1]
 	},
-	["207030"] = {
-		"DF. Thaldraszus /way #2025 51.0 56.6 Shi Everbreeze", -- [1]
+	["20933"] = {
+		"Vanilla TAQ T2.5 Armor for Priest, Mage, Warlock, Druid", -- [1]
 	},
 	["2924"] = {
 		"Vanilla", -- [1]
@@ -495,16 +494,15 @@ CIN_Save = {
 	["12206"] = {
 		"Vanilla", -- [1]
 	},
-	["20933"] = {
-		"Vanilla TAQ T2.5 Armor for Priest, Mage, Warlock, Druid", -- [1]
+	["207030"] = {
+		"DF. Thaldraszus /way #2025 51.0 56.6 Shi Everbreeze", -- [1]
 	},
 	["202017"] = {
 		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
 		"The Azure Span", -- [2]
 	},
-	["32569"] = {
-		"TBC", -- [1]
-		"Ogrila blade's edge", -- [2]
+	["45079"] = {
+		"safe to delete after loch modan quest complete", -- [1]
 	},
 	["35562"] = {
 		"Vanilla", -- [1]
@@ -512,21 +510,21 @@ CIN_Save = {
 	["2319"] = {
 		"Vanilla", -- [1]
 	},
-	["2672"] = {
-		"Vanilla", -- [1]
+	["128860"] = {
+		"LEG", -- [1]
 	},
 	["15416"] = {
 		"Vanilla", -- [1]
 	},
-	["6358"] = {
+	["18562"] = {
 		"Vanilla", -- [1]
 	},
-	["5637"] = {
+	["4402"] = {
 		"Vanilla", -- [1]
-		"LW, Smith, Jewel, Alchemy, Enchant", -- [2]
 	},
-	["22457"] = {
-		"TBC", -- [1]
+	["33865"] = {
+		"TBC,CTM", -- [1]
+		"Zulaman", -- [2]
 	},
 	["20858"] = {
 		"Vanilla", -- [1]
@@ -534,23 +532,23 @@ CIN_Save = {
 	["3857"] = {
 		"Vanilla", -- [1]
 	},
-	["2592"] = {
-		"Vanilla", -- [1]
+	["8154"] = {
+		"Vanilla. LW Tough Scorpid set. Thick Leather + This + Silk Thread", -- [1]
 	},
-	["2886"] = {
+	["13755"] = {
 		"Vanilla", -- [1]
 	},
 	["14256"] = {
 		"Vanilla", -- [1]
 	},
-	["20873"] = {
+	["2318"] = {
 		"Vanilla", -- [1]
 	},
-	["12803"] = {
+	["4304"] = {
 		"Vanilla", -- [1]
 	},
-	["8165"] = {
-		"Vanilla", -- [1]
+	["21877"] = {
+		"TBC", -- [1]
 	},
 	["200613"] = {
 		"DF, Teleport to Oharan centuar's camp", -- [1]
@@ -558,24 +556,24 @@ CIN_Save = {
 	["21237"] = {
 		"Vanilla TAQ weapon quest. Needs 3 Elementium Ingots.", -- [1]
 	},
-	["199219"] = {
-		"DF", -- [1]
-		"Buy Chip @ Waking Shores /way #2022 26.4 55.4 Chip", -- [2]
+	["162571"] = {
+		"BFA quest", -- [1]
 	},
-	["20865"] = {
-		"Vanilla", -- [1]
-	},
-	["2325"] = {
-		"Vanilla", -- [1]
-	},
-	["20424"] = {
-		"Vanilla", -- [1]
-	},
-	["20860"] = {
-		"Vanilla", -- [1]
-	},
-	["22450"] = {
+	["22449"] = {
 		"TBC", -- [1]
+	},
+	["5470"] = {
+		"Vanilla", -- [1]
+		"Cooking", -- [2]
+	},
+	["2770"] = {
+		"Vanilla", -- [1]
+	},
+	["12184"] = {
+		"Vanilla", -- [1]
+	},
+	["71634"] = {
+		"Azure Span /way #2024 16.57, 38.31", -- [1]
 	},
 	["52719"] = {
 		"CTM", -- [1]
@@ -583,20 +581,21 @@ CIN_Save = {
 	["20881"] = {
 		"Vanilla", -- [1]
 	},
-	["20864"] = {
+	["3712"] = {
 		"Vanilla", -- [1]
 	},
-	["2449"] = {
+	["202018"] = {
+		"Hand these in at /way #2024 66.93, 12.77 Sonova Snowden", -- [1]
+		"The Azure Span", -- [2]
+	},
+	["6291"] = {
 		"Vanilla", -- [1]
 	},
-	["6889"] = {
+	["2842"] = {
 		"Vanilla", -- [1]
 	},
-	["7078"] = {
+	["13759"] = {
 		"Vanilla", -- [1]
-	},
-	["21232"] = {
-		"Vanilla TAQ weapon quest. Needs 3 elementium ingots", -- [1]
 	},
 	["20931"] = {
 		"Vanilla T2.5 pants for Paladin, Hunter, Shaman, Warlock, Druid", -- [1]
@@ -604,11 +603,11 @@ CIN_Save = {
 	["22445"] = {
 		"TBC", -- [1]
 	},
-	["17010"] = {
+	["2673"] = {
 		"Vanilla", -- [1]
 	},
-	["162580"] = {
-		"BFA quest", -- [1]
+	["7081"] = {
+		"Vanilla", -- [1]
 	},
 	["12202"] = {
 		"Vanilla", -- [1]
@@ -616,20 +615,21 @@ CIN_Save = {
 	["20930"] = {
 		"VANILLA. TAQ T2.5 Helmet for Paladin, Hunter, Rogue, Shaman, Druid", -- [1]
 	},
-	["7080"] = {
+	["20862"] = {
 		"Vanilla", -- [1]
 	},
-	["7069"] = {
+	["12662"] = {
 		"Vanilla", -- [1]
 	},
-	["4338"] = {
+	["12804"] = {
 		"Vanilla", -- [1]
 	},
 	["162584"] = {
 		"BFA quest", -- [1]
 	},
-	["765"] = {
-		"Vanilla", -- [1]
+	["24291"] = {
+		"TBC", -- [1]
+		"sporeman rep turn in", -- [2]
 	},
 	["199906"] = {
 		"DF Turned in at Sorotis (in Valdrakken - 26,40)", -- [1]
